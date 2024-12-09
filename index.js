c8f69bbabaca2071ead422e8f608c938f5d8f150
RAMZ555 <sriramsubramaniyan555@gmail.com>
2024-12-09 11:16:48 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var sidenav = document.querySelector(".side-navbar");
+
+function shownavbar() {
+    sidenav.classList.add("active");
+}
+
+function closenavbar() {
+    sidenav.classList.remove("active");
+}
