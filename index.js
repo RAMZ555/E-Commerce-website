4d6f017c1293041b2bd44825c54befbf88a6be93
RAMZ555 <sriramsubramaniyan555@gmail.com>
2025-07-10 10:26:09 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var sidenav = document.querySelector(".side-navbar");
-
-function shownavbar() {
-    sidenav.classList.add("active");
-}
-
-function closenavbar() {
-    sidenav.classList.remove("active");
-}
